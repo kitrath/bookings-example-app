4e2e38cc22bbec1d617a67a30b7ffda2fa309e6b
kitrath <crathmel@gmail.com>
2023-05-30 17:15:06 -0500

    challenge 2.2, add user names to UserPicker

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,7 +1,23 @@
+import data from "../../static.json";
+
 export default function UserPicker() {
+  const { users } = data;
+
   return (
     <select>
-      <option>Users</option>
+      <option
+        value="" 
+      >
+        Users
+      </option>
+      {users.map((user) => (
+        <option
+          key={user.id}
+          value={user.name} 
+        >
+          {user.name}
+        </option>
+      ))}
     </select>
   );
 }
