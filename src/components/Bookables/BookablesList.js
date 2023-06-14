00239c8a2603cc2266a2932fd62f2985b79ddaa4
kitrath <crathmel@gmail.com>
2023-06-14 16:59:41 -0500

    lift state to BookablesView, remove reducer

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,77 +1,53 @@
-import { useReducer, useEffect, useRef, Fragment } from "react";
-// Still need to import file data for sessions, days
-import data from "../../static.json";
+import { useState, useEffect, useRef } from "react";
 import { FaArrowRight } from "react-icons/fa";
-
 import Spinner from "../UI/Spinner";
-import reducer from "./reducer";
 import getData from "../../utils/api";
 
-const { sessions, days } = data;
+export default function BookablesList({ bookable, setBookable }) {
+  const [bookables, setBookables] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-const initialState = {
-  group: "Rooms",
-  bookableIndex: 0,
-  hasDetails: true,
-  bookables: [],
-  isLoading: true,
-  error: false
-};
-
-export default function BookablesList() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const { group, bookableIndex, bookables } = state;
-  const { hasDetails, isLoading, error } = state;
+  const group = bookable?.group;
 
   const bookablesInGroup = bookables.filter(b => b.group === group);
-  const bookable = bookablesInGroup[bookableIndex];
   const groups = [...new Set(bookables.map(b => b.group))];
  
   const nextButtonRef = useRef();
 
   useEffect(() => {
-
-    dispatch({ type: "FETCH_BOOKABLES_REQUEST" });
-
     getData("http://localhost:3001/bookables")
 
-      .then((bookables) => {
-        // console.log("In fetch .then()", bookables);
-        dispatch({
-          type: "FETCH_BOOKABLES_SUCCESS",
-          payload: bookables
-        })
+      .then(bookables => {
+        setBookable(bookables[0]);
+        setBookables(bookables);
+        setIsLoading(false);
       })
 
-      .catch(error => dispatch({
-        type: "FETCH_BOOKABLES_ERROR",
-        payload: error
-      }));
+      .catch(error => {
+        setError(error);
+        setIsLoading(false);
+      });
 
-  }, []);
+  }, [setBookable]);
 
   function changeGroup(e) {
-    dispatch({
-      type: "SET_GROUP",
-      payload: e.target.value
-    });
+    const bookablesInSelectedGroup = bookables.filter(
+      b => b.group === e.target.value
+    );
+    setBookable(bookablesInSelectedGroup[0]);
   }
 
-  function changeBookable(selectedIndex) {
-    dispatch({
-      type: "SET_BOOKABLE",
-      payload: selectedIndex
-    });
+  function changeBookable(selectedBookable) {
+    setBookable(selectedBookable)
     nextButtonRef.current.focus();
   }
 
   function nextBookable() {
-    dispatch({ type: "NEXT_BOOKABLE" });
-  }
-
-  function toggleDetails() {
-    dispatch({ type: "TOGGLE_HAS_DETAILS" });
+    const i = bookablesInGroup.indexOf(bookable);
+    const nextIndex = (i + 1) % bookablesInGroup.length;
+    const nextBookable = bookablesInGroup[nextIndex];
+    setBookable(nextBookable);
   }
 
   if (error) {
@@ -83,84 +59,40 @@ export default function BookablesList() {
   }
 
   return (
-    <Fragment>
-      <div>
-        <select
-          value={group}
-          onChange={changeGroup}  
-        >
-          {groups.map(g => <option value={g} key={g}>{g}</option>)}
-        </select>
-        <ul className="bookables items-list-nav">
-          {bookablesInGroup.map((b, i) => (
-            <li
-              key={b.id}
-              className={ i === bookableIndex ? "selected" : null}
-            >
-              <button
-                className="btn"
-                onClick={() => changeBookable(i)}
-              >
-                {b.title}
-              </button>
-            </li>
-          ))}
-        </ul>
-        <p>
-          <button
-            className="btn"
-            onClick={nextBookable}
-            ref={nextButtonRef}
-            autofocus
+    <div>
+      <select
+        value={group}
+        onChange={changeGroup}  
+      >
+        {groups.map(g => <option value={g} key={g}>{g}</option>)}
+      </select>
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map(b => (
+          <li
+            key={b.id}
+            className={ b.id === bookable.id ? "selected" : null}
           >
-            <FaArrowRight/>
-            <span>Next</span>
-          </button>
-        </p>
-      </div>
+            <button
+              className="btn"
+              onClick={() => changeBookable(b)}
+            >
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button
+          className="btn"
+          onClick={nextBookable}
+          ref={nextButtonRef}
+          autofocus
+        >
+          <FaArrowRight/>
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
 
-      {bookable && (
-        <div className="bookable-details">
-          <div className="item">
-            <div className="item-header">
-              <h2>
-                {bookable.title}
-              </h2>
-              <span className="controls">
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={hasDetails}
-                    onChange={toggleDetails}
-                  />
-                  Show Details
-                </label>
-              </span>
-            </div>
-
-            <p>{bookable.notes}</p>
-
-            {hasDetails && (
-              <div className="item-details">
-                <h3>Availability</h3>
-                <div className="bookable-availability">
-                  <ul>
-                    {bookable.days
-                      .sort()
-                      .map(d => <li key={d}>{days[d]}</li>) 
-                    }
-                  </ul>
-                  <ul>
-                    {bookable.sessions
-                      .map(s => <li key={s}>{sessions[s]}</li>) 
-                    }
-                  </ul>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      )}
-    </Fragment>
   );
 }
