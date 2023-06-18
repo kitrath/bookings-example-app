b65a8ba6274bc0156b2ecf46e3abda1f67f5e8b6
kitrath <crathmel@gmail.com>
2023-06-18 08:15:00 -0500

    add updater function validation and useCallback to BookableView

--- src/components/Bookables/BookablesView.js
+++ src/components/Bookables/BookablesView.js
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState, useCallback, Fragment } from "react";
 
 import BookablesList from "./BookablesList";
 import BookableDetails from "./BookableDetails";
@@ -6,9 +6,16 @@ import BookableDetails from "./BookableDetails";
 export default function BookablesView() {
   const [bookable, setBookable] = useState();
 
+  const updateBookable = useCallback(selected => {
+    if (selected) {
+      selected.lastShown = Date.now();
+      setBookable(selected);
+    }
+  }, []);
+
   return (
     <Fragment>
-      <BookablesList bookable={bookable} setBookable={setBookable}/>
+      <BookablesList bookable={bookable} setBookable={updateBookable}/>
       <BookableDetails bookable={bookable}/>
     </Fragment>
   );
