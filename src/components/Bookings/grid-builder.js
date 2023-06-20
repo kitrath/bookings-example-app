b5d9b5579ea9384c1b989c2644b5f2e4a98715fa
kitrath <crathmel@gmail.com>
2023-06-20 16:56:33 -0500

    add grid builder

---
+++ src/components/Bookings/grid-builder.js
@@ -0,0 +1,29 @@
+import { sessions as sessionNames } from "../../static.json";
+import { addDays, shortISO } from "../../utils/date-wrangler";
+
+export function getGrid(bookable, startDate) {
+
+  const dates = bookable.days.sort().map(
+    d => shortISO(addDays(startDate, d))
+  );
+
+  const sessions = bookable.sessions.map(i => sessionNames[i]);
+
+  const grid = {};
+
+  sessions.forEach(session => {
+    grid[session] = {};
+    dates.forEach(date => grid[session][date] = {
+      session,
+      date,
+      bookableId: bookable.id,
+      title: ""
+    });
+  });
+
+  return {
+    grid,
+    dates,
+    sessions
+  };
+}
