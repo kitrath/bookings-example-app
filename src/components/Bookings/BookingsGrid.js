8f2ae30eec5065a0e9bbef938822e498f7ad2dc9
kitrath <crathmel@gmail.com>
2023-06-27 16:53:17 -0500

    add vars, effects to BookingsGrid

--- src/components/Bookings/BookingsGrid.js
+++ src/components/Bookings/BookingsGrid.js
@@ -1,5 +1,42 @@
-export default function BookingsGrid(props) {
-  const { week, bookable, booking, setBooking } = props;
+import { useEffect, useMemo, useState, Fragment } from "react";
+
+import { getGrid, transformBookings } from "./grid-builder";
+
+import { getBookings } from "../../utils/api";
+
+import Spinner from "../UI/Spinner";
+
+export default function BookingsGrid(
+  { week, bookable, booking, setBooking }
+) {
+  const [bookings, setBookings] = useState(null);
+  const [error, setError] = useState(false);
+
+  const {grid, sessions, dates} = useMemo(
+    () => bookable ? getGrid(bookable, week.start) : {},
+    [bookable, week.start]
+  );
+
+  useEffect(() => {
+    if (bookable) {
+      // track whether the bookings data is current
+      let doUpdate = true;
+
+      setBookings(null);
+      setError(false);
+      setBooking(null);
+
+      getBookings(bookable.id, week.start, week.end)
+        .then(resp => {
+          if (doUpdate) {
+            setBookings(transformBookings(resp));
+          }
+        })
+        .catch(setError);
+
+      return () => doUpdate = false;
+    }
+  }, [week, bookable, setBooking]);
 
   return (
     <div className="bookings-grid placeholder">
